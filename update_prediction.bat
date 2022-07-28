854be4b2bb9cd6a47a713abfa35e8e477c9ed871
pyh <pyh@example.com>
2022-07-28 15:01:25 +0800

    update0728 logs 1.PLAN TO BUILD A GENERAL MODEL

--- update_prediction.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-CALL D:\software\NORMAL\anaconda\anaconda\Scripts\activate.bat D:\software\NORMAL\anaconda\anaconda
-cd D:\my\dissertation\Treasury Securities Pre\bond_prediction-main
-python update_prediction.py
